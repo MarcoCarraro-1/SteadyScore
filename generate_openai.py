b2448e5b666ee2df58d2d49ad730f670e30338ef
Marco <marco.carraro-1@studenti.unitn.it>
2024-12-19 16:53:22 +0100

    Tutto fino a generazione domande e stampa prima domanda

--- generate_openai.py
+++ generate_openai.py
@@ -1,7 +1,7 @@
 from openai import OpenAI
 
 client = OpenAI(
-  api_key="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"
+  api_key=""
 )
 
 completion = client.chat.completions.create(
